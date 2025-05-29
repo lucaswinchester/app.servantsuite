a7bad2b9372b891af544383b03667c6f1cca8d33
Lucas Winchester <lwinchester@revgennetworks.com>
2025-05-29 13:44:55 -0500

    better auth pages

--- src/components/auth/AnimatedGradient.tsx
+++ src/components/auth/AnimatedGradient.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../../styles/animation.css';
 
-const AnimatedBackground = () => {
+const AnimatedGradient = () => {
   return (
-    <>
+    <div className="fixed inset-0 -z-10">
       <div className="animated-gradient"></div>
-      <div className="fixed inset-0 -z-10 bg-gradient-to-br from-white/5 via-transparent to-black/5"></div>
-    </>
+      <div className="absolute inset-0 bg-gradient-to-br from-white/5 via-transparent to-black/5"></div>
+    </div>
   );
 };
 
-export default AnimatedBackground;
-
+export default AnimatedGradient;
